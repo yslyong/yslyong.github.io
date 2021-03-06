d196f4d8566d426d7434b1560392ac800c108f55
yslyong <haorenwang99@gmail.com>
2021-03-06 16:12:20 +0800

    Contact Us done

--- js/default.js
+++ js/default.js
@@ -1,3 +1,27 @@
 window.onload = function () {
-  console.log(location.pathname);
+  const currentLocation = location.pathname;
+  const navLink = document.getElementsByClassName("nav-link");
+
+  Array.from(navLink).forEach((e) => {
+    if (e.getAttribute("href") === currentLocation) {
+      e.classList.add("text-primary");
+    } else {
+      e.classList.add("text-secondary");
+    }
+  });
+
+  const form = document.getElementById("contact");
+  if (form !== null) {
+    document.forms["contact"].reset();
+  }
 };
+
+function popmsg() {
+  const form = document.getElementById("contact");
+  console.log(form);
+  form.classList.add("d-none");
+
+  const popmsg = document.getElementById("popmsg");
+  console.log(popmsg);
+  popmsg.classList.remove("d-none");
+}
